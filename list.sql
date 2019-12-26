c9b6090fac75723771dfd49bec0fcea45c5c02bb
Swaathi K <swaathik2020@srishakthi.ac.in>
2019-12-26 11:39:00 +0530

    Update list.sql

--- list.sql
+++ list.sql
@@ -5,6 +5,7 @@ task_no number,
   deadline_date date,
   completed_date date,
   status varchar2(10),
+  priority_no number,
   constraint task_no_pk primary key(task_no),
   constraint status_cq check(status in('completed','bending'))
 );
@@ -13,11 +14,14 @@ insert into list(task_no,
                  emp_name,
                  deadline_date,
                  completed_date,
-                 status) values (1,'Install Oracle Software','Swaathi',to_date('20-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
+                 priority_no,
+                 status) values (1,'Install Oracle Software','Swaathi',to_date('20-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed,1);
 insert into list(task_no,
                  task_name,
                  emp_name,
                  deadline_date,
-                 completed_date,status) values (2,'Install jdk Software','Sruthi',to_date('21-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
+                 completed_date,
+                 priority_no,
+                 status) values (2,'Install jdk Software','Sruthi',to_date('21-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed.2);
 
-select *from list where status='completed';
+select *from list Order by priority_no;
