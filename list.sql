41244f208f0033c7c5f218ac3f7fff5be7d05fb6
Swaathi K <swaathik2020@srishakthi.ac.in>
2019-12-26 11:30:53 +0530

    Update list.sql

--- list.sql
+++ list.sql
@@ -8,7 +8,16 @@ task_no number,
   constraint task_no_pk primary key(task_no),
   constraint status_cq check(status in('completed','bending'))
 );
-insert into list(task_no,task_name,emp_name,deadline,completed_date,status) values (1,'Install Oracle Software','Swaathi',to_date('20-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
-insert into list(task_no,task_name,emp_name,deadline,completed_date,status) values (2,'Install jdk Software','Sruthi',to_date('21-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
+insert into list(task_no,
+                 task_name,
+                 emp_name,
+                 deadline_date,
+                 completed_date,
+                 status) values (1,'Install Oracle Software','Swaathi',to_date('20-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
+insert into list(task_no,
+                 task_name,
+                 emp_name,
+                 deadline_date,
+                 completed_date,status) values (2,'Install jdk Software','Sruthi',to_date('21-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
 
 select *from list where status='completed';
