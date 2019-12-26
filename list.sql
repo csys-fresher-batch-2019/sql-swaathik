b42b27d81afbe2d4789a331dd78f6141755b1c5d
Swaathi K <swaathik2020@srishakthi.ac.in>
2019-12-26 11:03:43 +0530

    Update list.sql

--- list.sql
+++ list.sql
@@ -1,5 +1,12 @@
 create table list(
-task_no number,task_name varchar2(50) not null,emp_name varchar2(50) not null,deadline date,completed_date date,status varchar2(10),constraint task_no_uq unique(task_no)
+task_no number,
+  task_name varchar2(50) not null,
+  emp_name varchar2(50) not null,
+  deadline_date date,
+  completed_date date,
+  status varchar2(10),
+  constraint task_no_pk primary key(task_no),
+  constraint status_cq check(status in('completed','bending'))
 );
 insert into list(task_no,task_name,emp_name,deadline,completed_date,status) values (1,'Install Oracle Software','Swaathi',to_date('20-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
 insert into list(task_no,task_name,emp_name,deadline,completed_date,status) values (2,'Install jdk Software','Sruthi',to_date('21-12-2019','dd-MM-yyyy'),to_date('20-12-2019','dd-MM-yyyy'),completed);
